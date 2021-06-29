a45e38709f54a3c90297a4953cbb86ef623ad48e
Rossen Boykov <rossen.boykov.uni@gmail.com>
2021-06-29 21:43:08 +0200

    s

--- client/API_inference.py
+++ client/API_inference.py
@@ -42,6 +42,10 @@ def main():
         length_b = struct.pack('I', length)
         timestamp('client', 'after_serialization')
 
+        print("L: " + task_name_length)
+        print("N: " + task_name)
+        print("CurM: " + cur_model)
+        print("CurD: " + cur_data)
         # Send Data
         client.send(task_name_length_b)
         client.send(task_name_b)
@@ -68,7 +72,7 @@ def main():
         latency_list.append(latency)
         latency = (time_2 - time_1) * 1000
         print("Inference request on machine X using model " + cur_model + " (" + str(batch_size) + " batchsize) completed for: " + str(latency) + "ms. ")
-        time.sleep(1)
+        time.sleep(2)
 
     
 
