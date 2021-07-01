663c6b8b56a537ec6ff65dc7faf421ac6b96b613
Rossen Boykov <rossen.boykov.uni@gmail.com>
2021-07-01 23:24:01 +0200

    asdasd

--- pipeswitch/frontend_tcp.py
+++ pipeswitch/frontend_tcp.py
@@ -18,7 +18,7 @@ class FrontendTcpThd(threading.Thread):
             type_len = struct.unpack('I', type_len_b)[0]
             type_b = self.agent.recv(type_len)
             type = type_b.decode()
-            print("type and type_len revieved at first point of contact!")
+            print("Type is : " + type)
 
             model_name_length_b = self.agent.recv(4)
             print("Recieved model name length in bytes:" + str(model_name_length_b) )
@@ -28,19 +28,17 @@ class FrontendTcpThd(threading.Thread):
                 break
             model_name_b = self.agent.recv(model_name_length)
             model_name = model_name_b.decode()
-            print("Pos 2")
+            
             self.qout.put((self.agent, model_name))
             timestamp('tcp', 'get_name')
 
             data_length_b = self.agent.recv(4)
             data_length = struct.unpack('I', data_length_b)[0]
-            print("Pos 3")
+            
             if data_length > 0:
                 data_b = self.agent.recv(data_length)
-                print("Pos 4a")
             else:
                 data_b = None
-                print("Pos 4b")
             timestamp('tcp', 'get_data')
             self.qout.put(data_b)
             timestamp('tcp', 'enqueue_request')
