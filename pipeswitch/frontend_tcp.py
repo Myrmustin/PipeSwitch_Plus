abf6ca0214dafe16b33aa67348499cd7f9e21f24
Rossen Boykov <rossen.boykov.uni@gmail.com>
2021-07-02 11:23:08 +0200

    asdasd

--- pipeswitch/frontend_tcp.py
+++ pipeswitch/frontend_tcp.py
@@ -28,8 +28,8 @@ class FrontendTcpThd(threading.Thread):
             regularFrontEndTcp(self)
 
             
-def regularFrontEndTcp(self):
-    model_name_length_b = self.agent.recv(4)
+def regularFrontEndTcp(self, self1):
+    model_name_length_b = self1.agent.recv(4)
     print("Recieved model name length in bytes:" + str(model_name_length_b) )
     model_name_length = struct.unpack('I', model_name_length_b)[0]
     print("Pos 1")
@@ -37,21 +37,21 @@ def regularFrontEndTcp(self):
     if model_name_length == 0:
         #break
         None
-    model_name_b = self.agent.recv(model_name_length)
+    model_name_b = self1.agent.recv(model_name_length)
     model_name = model_name_b.decode()
             
-    self.qout.put((self.agent, model_name))
+    self1.qout.put((self1.agent, model_name))
     timestamp('tcp', 'get_name')
 
-    data_length_b = self.agent.recv(4)
+    data_length_b = self1.agent.recv(4)
     data_length = struct.unpack('I', data_length_b)[0]
             
     if data_length > 0:
-        data_b = self.agent.recv(data_length)
+        data_b = self1.agent.recv(data_length)
     else:
         data_b = None
     timestamp('tcp', 'get_data')
-    self.qout.put(data_b)
+    self1.qout.put(data_b)
     timestamp('tcp', 'enqueue_request')
 
 
