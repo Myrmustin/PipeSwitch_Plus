7cc43e55d0830565bdfb7984fd297d67faf704b5
Rossen Boykov <rossen.boykov.uni@gmail.com>
2021-07-03 16:05:01 +0200

    asdasd

--- pipeswitch/frontend_schedule.py
+++ pipeswitch/frontend_schedule.py
@@ -30,7 +30,7 @@ class FrontendScheduleThd(threading.Thread):
         while True:
             #---------------------Take Type -----------
             #------------------------------------
-            self.regularFrontendScheduler(models, cuda_stream_for_parameter)
+            regularFrontendScheduler(self, models, cuda_stream_for_parameter)
             
             """# Get request
             agent, model_name = self.qin.get()
@@ -116,56 +116,56 @@ class FrontendScheduleThd(threading.Thread):
                     e.synchronize()
                 param_trans_pipe.send(mod_list[0])
 
-    def regularFrontendScheduler(self, models, cuda_stream_for_parameter):
+def regularFrontendScheduler(self, models, cuda_stream_for_parameter):
         
-        # Get request
-        agent, model_name = self.qin.get()
-        timestamp('schedule', 'get_request')
+    # Get request
+    agent, model_name = self.qin.get()
+    timestamp('schedule', 'get_request')
 
-        print('regularFrontendScheduler() runing!')
-        # Get current worker
-        _, _, _, term_pipe = self.worker_list[self.cur_w_idx]
-        timestamp('schedule', 'get_current_worker')
-            
-            
-        # Send terminate signal to current worker
-        term_pipe.send('terminate')
+    print('regularFrontendScheduler() runing!')
+    # Get current worker
+    _, _, _, term_pipe = self.worker_list[self.cur_w_idx]
+    timestamp('schedule', 'get_current_worker')
+        
+        
+    # Send terminate signal to current worker
+    term_pipe.send('terminate')
 
-        # Get next worker to work on request
-        self.cur_w_idx += 1
-        self.cur_w_idx %= len(self.worker_list)
-        new_pipe, _, param_trans_pipe_parent, _ = self.worker_list[self.cur_w_idx]
+    # Get next worker to work on request
+    self.cur_w_idx += 1
+    self.cur_w_idx %= len(self.worker_list)
+    new_pipe, _, param_trans_pipe_parent, _ = self.worker_list[self.cur_w_idx]
 
-        # Send request to new worker
-        new_pipe.send((agent, model_name))
-        timestamp('schedule', 'notify_new_worker')
+    # Send request to new worker
+    new_pipe.send((agent, model_name))
+    timestamp('schedule', 'notify_new_worker')
 
-        # Wait for current worker to terminate
-        resp = term_pipe.recv()
-        timestamp('schedule', 'terminate_current_worker')
+    # Wait for current worker to terminate
+    resp = term_pipe.recv()
+    timestamp('schedule', 'terminate_current_worker')
 
-        # Transfer data to GPU
-        data_b = self.qin.get()
-        new_pipe.send(data_b)
-        timestamp('schedule', 'send_data')
+    # Transfer data to GPU
+    data_b = self.qin.get()
+    new_pipe.send(data_b)
+    timestamp('schedule', 'send_data')
 
-        # Allocate cache to streams
-        with torch.cuda.stream(cuda_stream_for_parameter):
-            torch.cuda.insert_shared_cache_for_parameter() # pylint: disable=no-member
-        timestamp('schedule', 'insert_cache')
-        # Transfer parameters to GPU
-        batched_parameter_list = models[hash(model_name)]
-        self._transfer_parameter(new_pipe,
-                                batched_parameter_list, 
-                                cuda_stream_for_parameter,
-                                param_trans_pipe_parent)
-        timestamp('schedule', 'transfer_parameters')
+    # Allocate cache to streams
+    with torch.cuda.stream(cuda_stream_for_parameter):
+        torch.cuda.insert_shared_cache_for_parameter() # pylint: disable=no-member
+    timestamp('schedule', 'insert_cache')
+    # Transfer parameters to GPU
+    batched_parameter_list = models[hash(model_name)]
+    self._transfer_parameter(new_pipe,
+                            batched_parameter_list, 
+                            cuda_stream_for_parameter,
+                            param_trans_pipe_parent)
+    timestamp('schedule', 'transfer_parameters')
 
-        # Clear status
-        with torch.cuda.stream(cuda_stream_for_parameter):
-            torch.cuda.clear_shared_cache() # pylint: disable=no-member
-        timestamp('schedule', 'clear_status')
+    # Clear status
+    with torch.cuda.stream(cuda_stream_for_parameter):
+        torch.cuda.clear_shared_cache() # pylint: disable=no-member
+    timestamp('schedule', 'clear_status')
 
-        # Recv response
-        res = new_pipe.recv()
-        timestamp('schedule', 'get_response')
+    # Recv response
+    res = new_pipe.recv()
+    timestamp('schedule', 'get_response')
